00d455c074cc132c16fc6b88b9c2c2dca0592a30
dghodse <ghodsednyaneshwar@gmail.com>
2021-06-13 15:49:24 +0530

    Updates

---
+++ precache-manifest.8a17f0c0eadfe41a6e132efcfc9f02cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29483250a3549723ea11fd07a00a365",
+    "url": "/React---Tour_Blog/index.html"
+  },
+  {
+    "revision": "3d520a261007c32847a6",
+    "url": "/React---Tour_Blog/static/css/main.f377203a.chunk.css"
+  },
+  {
+    "revision": "1d375e953aeeb63aa2dd",
+    "url": "/React---Tour_Blog/static/js/2.c5ae91ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React---Tour_Blog/static/js/2.c5ae91ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d520a261007c32847a6",
+    "url": "/React---Tour_Blog/static/js/main.a7105582.chunk.js"
+  },
+  {
+    "revision": "9e54862c40ac60fccd1d",
+    "url": "/React---Tour_Blog/static/js/runtime-main.b1a63556.js"
+  },
+  {
+    "revision": "6f6e12814e56e901e2590bf12a34e76b",
+    "url": "/React---Tour_Blog/static/media/Spinner.6f6e1281.svg"
+  }
+]);
